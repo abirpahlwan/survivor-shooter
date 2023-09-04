8841f375e75d383a4b390ce2f40c1c6c6c484faa
abirpahlwan <abir978@gmail.com>
2023-09-05 05:32:01 +0600

    player movement

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private PlayerMovement movement;
+    private SpriteRenderer playerSprite;
+    
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        movement = GetComponent<PlayerMovement>();
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (movement.moveDirection.x != 0 || movement.moveDirection.y != 0) {
+            if (Mathf.Abs(movement.moveDirection.x) > Mathf.Abs(movement.moveDirection.y)) {
+                animator.SetBool("MoveLeftRight", true);
+                animator.SetBool("MoveUp", false);
+                animator.SetBool("MoveDown", false);
+                SetSpriteDirection();
+            } else {
+                animator.SetBool("MoveLeftRight", false);
+                animator.SetBool(movement.moveDirection.y > 0 ? "MoveUp" : "MoveDown", true);
+            }
+        }
+        else {
+            animator.SetBool("MoveLeftRight", false);
+            animator.SetBool("MoveUp", false);
+            animator.SetBool("MoveDown", false);
+        }
+    }
+
+    void SetSpriteDirection()
+    {
+        playerSprite.flipX = movement.moveDirection.x > 0;
+    }
+}
