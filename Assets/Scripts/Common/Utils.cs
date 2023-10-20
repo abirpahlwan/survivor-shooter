40e316c16c1d1ea18bafd45c96942542633488cf
abirpahlwan <abir978@gmail.com>
2023-10-20 18:30:52 +0600

    knife direction

---
+++ Assets/Scripts/Common/Utils.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Utils
+{
+    public static float GetAngleFromDirection(Vector2 direction)
+    {
+        float dirx = direction.x;
+        float diry = direction.y;
+        
+        if (dirx > 0 && diry == 0) //right
+        {
+            return 0f;
+        }
+        if (dirx > 0 && diry > 0) //right up
+        {
+            return 45f;
+        }
+        if (dirx == 0 && diry > 0) //up
+        {
+            return 90f;
+        }
+        if (dirx < 0 && diry > 0) //left up
+        {
+            return 135f;
+        }
+        if (dirx < 0 && diry == 0) //left
+        {
+            return 180f;
+        }
+        if (dirx < 0 && diry < 0) //left down
+        {
+            return -135f;
+        }
+        if (dirx == 0 && diry < 0) //down
+        {
+            return -90f;
+        }
+        if (dirx > 0 && diry < 0) //right down
+        {
+            return -45f;
+        }
+        
+        return 0;
+    }
+}
--- Assets/Scripts/Weapons/WeaponBase/ProjectileWeaponBase.cs
+++ Assets/Scripts/Weapons/WeaponBase/ProjectileWeaponBase.cs
@@ -18,48 +18,9 @@ public class ProjectileWeaponBase : MonoBehaviour
     public void SetDirection(Vector3 dir)
     {
         direction = dir;
-
-        float dirx = direction.x;
-        float diry = direction.y;
-
-        Vector3 scale = transform.localScale;
+        
         Vector3 rotation = transform.rotation.eulerAngles;
-
-        if (dirx < 0 && diry == 0) //left
-        {
-            scale.x = scale.x * -1;
-            scale.y = scale.y * -1;
-        }
-        else if (dirx == 0 && diry < 0) //down
-        {
-            scale.y = scale.y * -1;
-        }
-        else if (dirx == 0 && diry > 0) //up
-        {
-            scale.x = scale.x * -1;
-        }
-        else if (dirx > 0 && diry > 0) //right up
-        {
-            rotation.z = 0f;
-        }
-        else if (dirx > 0 && diry < 0) //right down
-        {
-            rotation.z = -90f;
-        }
-        else if (dirx < 0 && diry > 0) //left up
-        {
-            scale.x = scale.x * -1;
-            scale.y = scale.y * -1;
-            rotation.z = -90f;
-        }
-        else if (dirx < 0 && diry < 0) //left down
-        {
-            scale.x = scale.x * -1;
-            scale.y = scale.y * -1;
-            rotation.z = 0f;
-        }
-
-        transform.localScale = scale;
+        rotation.z = Utils.GetAngleFromDirection(direction);
         transform.rotation = Quaternion.Euler(rotation); 
     }
 }
